bd508470671a3da55c6dd3ac8fc5b5febfcf2e2b
nishchayDipte <164716811+nishchayDipte@users.noreply.github.com>
2025-08-27 12:02:25 +0000

    conversion of data types

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state; // undefined (value is not yet assign)
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value its not zero (its type of object)
+// undefined => 
+// symbol => unique(used mostly in react give uniqunessto each)
+
+
+// object
+//typeof use to know type of data type
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
---
+++ 01_basics/vairables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+//canot cahnge const values
+
+// in js we can define vairable without variable declaration keywords(let,const,var) by default consider let
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
